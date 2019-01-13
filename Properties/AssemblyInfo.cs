58e7d1dac738be5db0d6112ef4ea519ef5252a03
Jaap de Haan <jaap.dehaan@alpine.de>
2019-01-13 15:33:13 +0100

    Basic change to .NET core

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,20 +7,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("ACQC.Metrics")]
 [assembly: AssemblyDescription("Computes Source Code Metrics")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jaap de Haan, color-of-code.de")]
 [assembly: AssemblyProduct("ACQC.Metrics")]
 [assembly: AssemblyCopyright("Copyright © Jaap de Haan 2006-2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9e0d48b3-959f-4d30-bb06-0a02ce624562")]
 
 // Version information for an assembly consists of the following four values:
 //
