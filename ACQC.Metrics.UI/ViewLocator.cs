60f2098d494ca62cb357a3a75f0493d21c8b8938
Jaap de Haan <jaap.dehaan@alpsalpine.com>
2022-11-14 17:28:02 +0100

    fix: Cleanup, move to .NET 6

--- ACQC.Metrics.UI/ViewLocator.cs
+++ ACQC.Metrics.UI/ViewLocator.cs
@@ -14,17 +14,15 @@ namespace ACQC.Metrics.UI
 
         public IControl Build(object data)
         {
+            if (data is null)
+                throw new ArgumentNullException(nameof(data));
+
             var name = data.GetType().FullName.Replace("ViewModel", "View");
             var type = Type.GetType(name);
 
-            if (type != null)
-            {
-                return (Control)Activator.CreateInstance(type);
-            }
-            else
-            {
-                return new TextBlock { Text = "Not Found: " + name };
-            }
+            return type != null
+                ? (Control)Activator.CreateInstance(type)
+                : (IControl)new TextBlock { Text = "Not Found: " + name };
         }
 
         public bool Match(object data)
